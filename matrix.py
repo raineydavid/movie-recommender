92ab6202b10a5e24ac99e92fe56339775aa9392c
tanmaiyii <34455315+tanmaiyii@users.noreply.github.com>
2017-12-14 14:38:07 +0000

    Update matrix.py

--- matrix.py
+++ matrix.py
@@ -1,14 +1,14 @@
-import pyspark
-
 import re
-def get_vector(str):
+def get_vector(input):
     if str:
-        dict = {'Action':0,'Adventure':1,'Animation':2,'Children':3,'Comedy':4,'Crime':5,'Documentary':6,'Drama':7,'Fantasy':8,
+        genres = {'Action':0,'Adventure':1,'Animation':2,'Children':3,'Comedy':4,'Crime':5,'Documentary':6,'Drama':7,'Fantasy':8,
                 'Film-Noir':9,'Horror':10,'IMAX':11,'Musical':12,'Mystery':13,'(no genres listed)':14,'Romance':15,
                 'Sci-Fi':16,'Thriller':17,'War':18,'Western':19}
-        rsplit = re.split(r '|', str)
+      #  input = re.split("|", input)
         vec = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-        for item in rsplit:
-            vec[dict['item']]= 1
+        for item in input:
+            mykey = str(item)
+            vec[genres[mykey]]= 1
     #return _compile(pattern, flags).split(string, maxsplit)
         return vec
+    
