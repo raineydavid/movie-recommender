fb0b170e370d5689a4a60377a122187ec7fcc759
SebCreighton <34573930+SebCreighton@users.noreply.github.com>
2017-12-15 17:51:52 +0000

    Update visualisation.py

--- visualisation.py
+++ visualisation.py
@@ -1,16 +1,28 @@
-import seaborn as sns
+
 import pandas as pd
 import numpy as np
 
-import matplotlib.pyplot as mp
+import matplotlib.pyplot as plt
 
 
 file='/homes/sc325/bigdata/ml-latest-small/ratings.csv'
 
 r_cols= ['userId', 'movieId', 'rating', 'timestamp']
 
-users = pd.read_csv(file, sep=',', names=r_cols)
+users = pd.read_csv(file, sep=',', names=r_cols, encoding='latin-1')
 
-users.head()
+users=users.drop(users.index[[0]])
+
+print users
+
+movieId_count=users['movieId'].value_counts()[:20]
 
 
+print movieId_count
+
+bar_graph=users['movieId'].value_counts()[:20].plot(kind='barh')
+
+plt.ylabel('MovieId Count')
+plt.xlabel('MovieId')
+plt.title('Bar Graph for MovieId Count')
+plt.show()
