170dabdf530443bef195b5f56a326aed3dbd7500
dede95 <adotu95@gmail.com>
2017-12-20 21:26:44 +0000

    Update similaritycos.py

--- similaritycos.py
+++ similaritycos.py
@@ -1,16 +1,3 @@
-def cosine_similarity(ratingPairs):
-     
-    numPairs = 0
-    sum_xx = sum_yy = sum_xy = 0
-        
-    for ratingX, ratingY in ratingPairs:
-         
-        sum_xx += ratingX * ratingX
-        sum_yy += ratingY * ratingY
-        sum_xy += ratingX * ratingY
-        numPairs += 1
-          
-          
 import numpy 
 from numpy import sqrt
 
