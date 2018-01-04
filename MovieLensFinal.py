5c555e5f32e5a0f1a343aeb4af0044a8d864d8ea
dede95 <adotu95@gmail.com>
2018-01-04 21:25:06 +0000

    Create MovieLensFinal.py
    Team effort - Adeola and Rainey

---
+++ MovieLensFinal.py
@@ -0,0 +1,122 @@
+import findspark
+findspark.init()
+
+
+import pyspark
+import sys
+import re
+import random
+#import numpy
+
+from pyspark import SparkConf, SparkContext
+sc = SparkContext(appName = "MovieLens")
+from math import sqrt
+#sc.addPyFile("similaritycos.py")
+sc.addPyFile("movielensfcn.py")
+#import similaritycos
+import movielensfcn
+from movielensfcn import parseMovies, removeDuplicates, itemItem
+#from similaritycos import cosine_similarity
+
+#def sqrt(number):
+    
+ #   return abs(number**(0.5))
+
+
+def cosine_similarity(ratingPairs):
+
+    numPairs = 0
+    sum_xx = sum_yy = sum_xy = 0
+
+    for ratingX, ratingY in ratingPairs:
+
+        sum_xx += ratingX * ratingX
+        sum_yy += ratingY * ratingY
+        sum_xy += ratingX * ratingY
+        numPairs += 1
+
+    numerator = sum_xy
+    denominator = sqrt(sum_xx) * sqrt(sum_yy)
+
+    score = 0
+    if (denominator):
+        score = ((float(numerator)) / (float(denominator)))
+
+    return (score, numPairs)
+
+
+
+
+if __name__=="__main__":
+    if len(sys.argv)< 3:
+        print >> sys.stderr, "Usage: MovieLens ratings movies"
+        exit(-1)
+    ratings_file = sys.argv[1]
+    movies_file = sys.argv[2]
+    if len(sys.argv)> 5:
+        movie_id = int(sys.argv[3])
+	threshold = float(sys.argv[4])
+	topN= int(sys.argv[5])
+        minOccurence = int(sys.argv[6])
+
+
+print '{0}, {1}, {2}, {3}, {4}'.format(ratings_file, movies_file, movie_id, threshold, topN)
+
+ratings_data = sc.textFile(ratings_file)
+movies_data = sc.textFile(movies_file)
+
+# data = sc.parallelize([(2.0,5.0), (2.5,4.5), (3.0,1.0), (5.0,2.0)])
+# data1 = data.map(cosine_similarity).saveAsTextFile("test1")
+
+ratings_header = ratings_data.take(1)[0]
+movies_header = movies_data.take(1)[0]
+
+movies= movies_data.filter(lambda line: line!=movies_header).map(lambda line: re.split(r',',line)).map(lambda x: (int(x[0]),(x[1],x[2])))
+
+ratings = ratings_data.filter(lambda line: line!=ratings_header).map(lambda line: re.split(r',',line)).map(lambda x: (int(x[0]),(int(x[1]),float(x[2])))).partitionBy(100)
+
+user_ratings_data = ratings.join(ratings)
+
+unique_joined_ratings = user_ratings_data.filter(removeDuplicates)
+
+
+
+movie_pairs = unique_joined_ratings.map(itemItem).partitionBy(100)
+
+movie_pairs_ratings= movie_pairs.groupByKey()
+
+item_item_similarities = movie_pairs_ratings.mapValues(cosine_similarity).persist()
+
+item_item_sorted=item_item_similarities.sortByKey()
+
+
+#item_item_sorted.saveAsTextFile("movie-similar0001")
+# Sample output
+#((1, 2), (0.9633070604343126, 71))
+#((1, 3), (0.9269097345177958, 39))
+#((1, 4), (0.932135764636765, 7))
+
+item_item_sorted.persist()
+
+# Filter for movies with this sim that are "good" as defined by
+# our quality thresholds above
+filteredResults = item_item_sorted.filter(lambda((item_pair,similarity_occurence)): \
+        (item_pair[0] == movie_id or item_pair[1] == movie_id) \
+        and similarity_occurence[0] > threshold and similarity_occurence[1] > minOccurence)
+
+if (topN==0):
+    topN=10
+
+results = filteredResults.map(lambda((x,y)): (y,x)).sortByKey(ascending = False)
+resultsTopN = results.take(topN)
+results.saveAsTextFile("top10test6")
+
+ #   print "Top 10 similar movies for " + nameDict[movieID]
+ #   for result in resultsTopN:
+ #       (sim, pair) = result
+        # Display the similarity result that isn't the movie we're looking at
+ #       similarMovieID = pair[0]
+ #       if (similarMovieID == movieID):
+ #           similarMovieID = pair[1]
+ #       print nameDict[similarMovieID] + "\tscore: " + str(sim[0]) + "\tstrength: " + str(sim[1])
+
