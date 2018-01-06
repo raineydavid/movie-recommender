3db041db14096147404064a03c1941bde9f384fd
dede95 <adotu95@gmail.com>
2018-01-06 15:46:03 +0000

    Update movielensfcn.py

--- movielensfcn.py
+++ movielensfcn.py
@@ -6,25 +6,26 @@ def parseVector(line):
 #     # line.split("::")moviePairSimilarities.saveAsTextFile("movie-sims")
 #     fields= parseVector(line)
 #     return int(fields[0]),(int(fields[1]),float(fields[2]))
-def loadMovieNames():
-    movieNames = {}
-    with open("/data/movie-ratings/movies.dat") as f:
-        for line in f:
-            fields = line.split("::")
-            movieNames[int(fields[0])] = fields[1].decode('ascii', 'ignore')
-    return movieNames
+
+# def loadMovieNames():
+#     movieNames = {}
+#     with open("/data/movie-ratings/movies.dat") as f:
+#         for line in f:
+#             fields = line.split("::")
+#             movieNames[int(fields[0])] = fields[1].decode('ascii', 'ignore')
+#     return movieNames
 
 def parseMovies(line,movieDict):
     fields= parseVector(line)
     movieDict[int(fields[0])] = fields[1]
     return fields[0],(fields[1],fields[2])
 
-def getSampleInteractions(user, film, n):
-    if len(film) > n:
-        return user, random.sample(film,n)
-    else:
-        return user, film
-    return
+# def getSampleInteractions(user, film, n):
+#     if len(film) > n:
+#         return user, random.sample(film,n)
+#     else:
+#         return user, film
+#     return
 
 def removeDuplicates((userid, ratings)): #movies the user has rated 
     (item1, value1) = ratings[0]
